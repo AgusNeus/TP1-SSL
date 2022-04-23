9fb6f8d045060676b0f7af34f1e6d7019d2272e2
AgusNeus <agtineus@gmail.com>
2022-04-23 12:22:23 -0300

    TP1 SSL

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <ctype.h>
+int main ()
+{
+int c;
+while(EOF!= (c=getchar()))
+{
+    if(isalpha(c)){
+        if(isupper(c)){
+
+            c=tolower(c);}
+
+        else if (islower(c)){
+
+            c=toupper(c);}
+        }
+    else if (isdigit(c)){
+
+        c=0;
+    }
+
+    if(c!=0)
+    putchar (c);
+
+}
+    
+
+return 0;
+}
